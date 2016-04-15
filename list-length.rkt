cd5c28f50332dcc30e769a8c961ffcb77cba3e53
Jones Melton <shavah@gmail.com>
2016-04-15 17:08:29 -0500

    Fix indexing error

--- list-length.rkt
+++ list-length.rkt
@@ -1,1 +1,11 @@
 #lang racket
+
+; list-length : list -> number
+(define (list-length list) (length list))
+
+; find-index : list item -> number
+; (define (find-index list item) ())
+
+(define (list-traverser list item index) (if (= (car list) item) (index) (list-traverser (cdr list) item (+ index 1))))
+
+
