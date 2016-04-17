3c22b6152435d380c39fb0117f003de2a901b3ac
Jones Melton & Eric Tenza <pair+shavah.tenzaej@devbootcamp.com>
2016-04-17 16:13:28 -0500

    Add lowercase validator

--- higherest-order-functions.rkt
+++ higherest-order-functions.rkt
@@ -1,4 +1,6 @@
 #lang racket
+(require mzlib/string)
+
 
 ;Write a function that triples all numbers in a list, filters out the odds, and sums the remaining numbers.
 ; Use Racket's higher order functions for this. foldl (filter (map))  map.filter.reduce
@@ -10,3 +12,11 @@
                  (map (λ (number)(* number 3)) list))))
 
 (triple-evens (list 1 2 3 2))
+
+; lowercase-validator list -> boolean
+(define (lowercase-validator list)
+  ;  (let ([x list]) x)
+  (equal? (list->set list) (list->set (map
+          (λ (word) (map (λ (letter) (char-downcase letter)) (string->list word)))list))))
+
+(lowercase-validator (list "hey"))
